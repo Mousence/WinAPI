2d4ccbdf10621b03f5d4e38a768df577f1266030
79141 <79141@DESKTOP-GMGD17F>
2023-12-16 20:15:30 +0900

    Window DONE

--- MainWindow/main.cpp
+++ MainWindow/main.cpp
@@ -1,13 +1,24 @@
+ï»¿#define _CRT_SECURE_NO_WARNINGS
 #include <Windows.h>
 #include <string>
+#include <CommCtrl.h>
+#include <mmiscapi.h>
 #include "resource.h"
 
 CONST CHAR g_sz_WINDOW_CLASS[] = "My first Window";
 
+#pragma comment(lib, "comctl32.lib")
+#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+TOOLINFO g_toolItem;
+HWND g_hwndTrackingTT;
+BOOL g_trackingMouse = FALSE;
+
 INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+HWND CreateTrackingToolTip(INT toolID, HWND hwnd, LPSTR lpText);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow) {
-	// 1) Ðåãèñòðàöèÿ êëàññà îêíà
+	// 1) Ð ÐµÐ³Ð¸ÑÑ‚Ñ€Ð°Ñ†Ð¸Ñ ÐºÐ»Ð°ÑÑÐ° Ð¾ÐºÐ½Ð°
 	WNDCLASSEX wc;
 	ZeroMemory(&wc, sizeof(wc));
 	//cb - Count Bytes
@@ -18,7 +29,7 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 
 	//wc.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
 	//wc.hIconSm = LoadIcon(hInstance, IDI_APPLICATION);
-	wc.hIcon = (HICON)LoadImage(hInstance, "icon_py_trash.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
+	wc.hIcon = (HICON)LoadImage(hInstance, "icon_py_is_trash.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
 	wc.hIconSm = (HICON)LoadImage(hInstance, "pycharm.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
 	//wc.hCursor = LoadCursor(hInstance, MAKEINTRESOURCE(IDC_CURSOR1));
 	wc.hCursor = (HICON)LoadImage(hInstance, "eyeball2.ani", IMAGE_CURSOR, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
@@ -34,15 +45,16 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 		return 0;
 	}
 
-	// 2) Ñîçäàíèå îêíà
-	int screenWidth = GetSystemMetrics(SM_CXSCREEN);
-	int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+	// 2) Ð¡Ð¾Ð·Ð´Ð°Ð½Ð¸Ðµ Ð¾ÐºÐ½Ð°
+	// SM - System Metrics
+	INT screenWidth = GetSystemMetrics(SM_CXSCREEN);
+	INT screenHeight = GetSystemMetrics(SM_CYSCREEN);
 
-	int windowWidth = screenWidth * 0.75;
-	int windowHeight = screenHeight * 0.75;
+	INT windowWidth = screenWidth * 0.75;
+	INT windowHeight = screenHeight * 0.75;
 
-	int windowX = (screenWidth - windowWidth) / 2;
-	int windowY = (screenHeight - windowHeight) / 2;
+	INT windowX = (screenWidth - windowWidth) / 2;
+	INT windowY = (screenHeight - windowHeight) / 2;
 
 	HWND hwnd = CreateWindowEx
 	(
@@ -57,7 +69,9 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 		hInstance,
 		NULL
 	);
-	// 3) Çàïóñê öèêëà ñîîáùåíèé
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+	// 3) Ð—Ð°Ð¿ÑƒÑÐº Ñ†Ð¸ÐºÐ»Ð° ÑÐ¾Ð¾Ð±Ñ‰ÐµÐ½Ð¸Ð¹
 	MSG msg;
 	while (GetMessage(&msg, NULL, 0, 0) > 0) {
 		TranslateMessage(&msg);
@@ -68,39 +82,96 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 
 INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	static RECT windowRect;
-
 	switch (uMsg)
 	{
-	case WM_MOVE:
-		GetWindowRect(hwnd, &windowRect);
-		break;
-	case WM_SIZE:
-		GetWindowRect(hwnd, &windowRect);
-		break;
-	case WM_PAINT:
-	{
-		PAINTSTRUCT ps;
-		HDC hdc = BeginPaint(hwnd, &ps);
-
-		// Display size and position in the title bar
-		std::string title = std::to_string(windowRect.right - windowRect.left) +
-			"x" + std::to_string(windowRect.bottom - windowRect.top) +
-			"\t" + std::to_string(windowRect.left) +
-			"x" + std::to_string(windowRect.top);
-
-		SetWindowText(hwnd, title.c_str());
-
-		EndPaint(hwnd, &ps);
-	}
 	case WM_CREATE:
-		break;
+		InitCommonControls();
+		g_hwndTrackingTT = CreateTrackingToolTip(IDC_TOOLTIP, hwnd, (LPSTR)"");
+		return TRUE;
+	case WM_MOUSELEAVE:
+		SendMessage(g_hwndTrackingTT, TTM_TRACKACTIVATE, (WPARAM)FALSE, (LPARAM)&g_toolItem);
+		g_trackingMouse = FALSE;
+		return FALSE;
+	case WM_MOUSEMOVE:
+		static INT oldX, oldY;
+		INT newX, newY;
+		if (!g_trackingMouse)
+		{
+			TRACKMOUSEEVENT tme = { sizeof(TRACKMOUSEEVENT) };
+			tme.hwndTrack = hwnd;
+			tme.dwFlags = TME_LEAVE;
+
+			TrackMouseEvent(&tme);
+
+			SendMessage(g_hwndTrackingTT, TTM_TRACKACTIVATE, (WPARAM)TRUE, (LPARAM)&g_toolItem);
+			g_trackingMouse = TRUE;
+		}
+		newX = LOWORD(lParam);
+		newY = HIWORD(lParam);
+
+		if (newX != oldX || newY != oldY)
+		{
+			oldX = newX;
+			oldY = newY;
+
+			CHAR coords[12]{};
+			sprintf(coords, "%ix%i", oldX, oldY);
+			g_toolItem.lpszText = coords;
+			SendMessage(g_hwndTrackingTT, TTM_SETTOOLINFO, 0, (LPARAM)&g_toolItem);
+
+			POINT pt = { oldX, oldY };
+			ClientToScreen(hwnd, &pt);
+			SendMessage(g_hwndTrackingTT, TTM_TRACKPOSITION, 0, (LPARAM)MAKELONG(pt.x + 10, pt.y - 20));
+		}
+		return FALSE;
+	case WM_SIZE:
+	case WM_MOVING:
+	{
+		CONST INT SIZE = 256;
+		CHAR sz_title[SIZE]{};
+		RECT rect;
+		GetWindowRect(hwnd, &rect);
+		sprintf(sz_title, "%s Position:%ix%i, Size: %ix%i", g_sz_WINDOW_CLASS,
+			rect.left, rect.top,
+			rect.right - rect.left, rect.bottom - rect.top);
+		SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_title);
+	}
+	break;
 	case WM_COMMAND:
 		break;
-	case WM_DESTROY: PostQuitMessage(0); break;
-	case WM_CLOSE: DestroyWindow(hwnd); break;
-	default:
-		return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	case WM_DESTROY:PostQuitMessage(0); break;
+	case WM_CLOSE:	DestroyWindow(hwnd); break;
+	default:		return DefWindowProc(hwnd, uMsg, wParam, lParam);
 	}
-	return FALSE;
+	return 0;
+}
+HWND CreateTrackingToolTip(INT	toolID, HWND hwnd, LPSTR lpszText)
+{
+	HWND hwndTT = CreateWindowEx(
+		WS_EX_TOPMOST, TOOLTIPS_CLASS, NULL,
+		WS_POPUP | TTS_NOPREFIX | TTS_ALWAYSTIP,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		hwnd,
+		NULL,
+		GetModuleHandle(NULL),
+		NULL
+	);
+	if (hwndTT == NULL)
+	{
+		MessageBox(hwnd, "Tooltip creation failed", "Error", MB_OK | MB_ICONERROR);
+		return NULL;
+	}
+
+	g_toolItem.cbSize = sizeof(TOOLINFO);
+	g_toolItem.uFlags = TTF_IDISHWND | TTF_TRACK | TTF_ABSOLUTE;
+	g_toolItem.hwnd = hwnd;
+	g_toolItem.hinst = GetModuleHandle(NULL);
+	g_toolItem.lpszText = lpszText;
+	g_toolItem.uId = (UINT_PTR)hwnd;
+	GetClientRect(hwnd, &g_toolItem.rect);
+
+	//Associate the tooltip with the tool window.
+	SendMessage(hwndTT, TTM_ADDTOOL, 0, (LPARAM)(LPTOOLINFO)&g_toolItem);
+	return hwndTT;
 }
